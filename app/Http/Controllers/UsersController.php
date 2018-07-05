27955f2e47888a31f086cb80d5c2ae8704463c62
Gio <giovani87@hotmail.com>
2018-07-05 19:10:28 -0300

    Users page working without photos

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\UsersRequest;
+use App\User;
+use App\Role;
+use Illuminate\Support\Facades\Hash;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+//        $roles = Role::lists('name', 'id')->all();
+        $roles = Role::all();
+        return view('admin.users.create', compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UsersRequest $request)
+    {
+        //return $request->all();
+//        $password = Hash::make($request->input('password'));
+//        return $password;
+        User::create($request->all());
+        return redirect('/users');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('admin.users.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Create Users</h1>
+    <form method="POST" action="/users" enctype="multipart/form-data">
+        <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control" name="name" placeholder="Enter Name" required>
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" class="form-control" name="password" placeholder="Enter password" required>
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" class="form-control" name="email" placeholder="Enter email" required>
+        </div>
+        <div class="form-group">
+            <label>Role</label>
+            <select class="form-control" name="role_id">
+                @foreach($roles as $role)
+                {{--<option value="1">Administrator</option>--}}
+                {{--<option value="0">Subscriber</option>--}}
+                    <option value='{{$role->id}}'>{{$role->name}}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="form-group">
+            <label>Status</label>
+            <select class="form-control" name="is_active">
+                <option value="1">Active</option>
+                <option value="0">Not Active</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label>Upload File</label>
+            <input type="file" class="form-control-file" name="photo_id">
+        </div>
+        <button type="submit" class="btn btn-primary">Create Post</button>
+        @csrf
+        {{--{{csrf_field()}}--}}
+    </form>
+
+    @include('includes.form_error')
+
+@stop
